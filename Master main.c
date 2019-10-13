f05191cd822f0b528ea32ce10dc05ab74c206dee
Abdelrahman Hadi <50929071+abdelrahman-hadi@users.noreply.github.com>
2019-10-13 22:57:08 +0200

    Update Master main.c

--- Master main.c
+++ Master main.c
@@ -16,7 +16,7 @@ Wire.begin(); // join i2c bus (address optional for master)
 pinMode(13, OUTPUT); //Set output 13 (LED) as output
 setup_mpu_6050_registers(); //Setup the registers of the MPU-6050
 digitalWrite(13, HIGH); //Set digital output 13 high to indicate startup
-for (int cal_int = 0; cal_int &lt; 2000 ; cal_int ++) { //Run this code 2000 times
+for (int cal_int = 0; cal_int < 2000 ; cal_int ++) { //Run this code 2000 times
   if (cal_int % 125 == 0)digitalWrite(13, !digitalRead(13)); //Print a dot on the LCD every 125 readings
 read_mpu_6050_data(); //Read the raw acc and gyro data from the MPU-6050
 gyro_x_cal += gyro_x; //Add the gyro x-axis offset to the gyro_x_cal variable
@@ -78,8 +78,8 @@ Wire.write(roll); // sends one byte
 Wire.write(pitch); // sends one byte
 Wire.write(yaw); // sends one byte
 Wire.endTransmission(); // stop transmitting
-while (micros() - loop_timer &lt; 4000); //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
-if (micros()- loop_timer &gt; 4000) digitalWrite(13,HIGH); // indication if the loop exceeds 4000us
+while (micros() - loop_timer < 4000); //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+if (micros()- loop_timer > 4000) digitalWrite(13,HIGH); // indication if the loop exceeds 4000us
 else digitalWrite (13,LOW);
 loop_timer = micros(); // calculate the constant loop time for integration
 }
@@ -89,14 +89,14 @@ Wire.beginTransmission(0x68); //Start communicating with the MPU-6050
 Wire.write(0x3B); //Send the requested starting register
 Wire.endTransmission(); //End the transmission
 Wire.requestFrom(0x68, 14); //Request 14 bytes from the MPU-6050
-while (Wire.available() &lt; 14); //Wait until all the bytes are received
-acc_x = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the acc_x variable
-acc_y = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the acc_y variable
-acc_z = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the acc_z variable
-temperature = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the temperature variable
-gyro_x = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the gyro_x variable
-gyro_y = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the gyro_y variable
-gyro_z = Wire.read() &lt;&lt; 8 | Wire.read(); //Add the low and high byte to the gyro_z variable
+while (Wire.available() < 14); //Wait until all the bytes are received
+acc_x = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the acc_x variable
+acc_y = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the acc_y variable
+acc_z = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the acc_z variable
+temperature = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the temperature variable
+gyro_x = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the gyro_x variable
+gyro_y = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the gyro_y variable
+gyro_z = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the gyro_z variable
 }
 
 void setup_mpu_6050_registers() {
